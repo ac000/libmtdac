68ccb42e2a69c7579e4f94fcc2ee2ae412f35e45
Andrew Clayton <andrew@digital-domain.net>
2020-05-27 15:50:06 +0100

    mtd-ic: Convert to do_ep()
    This makes use of the infrastructure added in commit 5ea027e
    ("endpoints: Add a generic do_ep() function") to convert all the
    mtd_ic_* functions.
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- src/mtd-ic.c
+++ src/mtd-ic.c
@@ -6,9 +6,10 @@
  * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
  */
 
+#include <stddef.h>
+
 #include "mtd-ic.h"		/* for default (public) visibility */
 #include "endpoints.h"
-#include "curler.h"
 
 #define API_VER			"Accept: application/vnd.hmrc.1.0+json"
 
@@ -18,14 +19,8 @@
  */
 int mtd_ic_get_messages(const char *cid, char **buf)
 {
-	struct curl_ctx ctx = { 0 };
-	const char *params[] = { cid };
-
-	ctx.mtd_api_ver = API_VER;
-	ctx.endpoint = IC_GET_MSGS;
-	ctx.params = params;
-
-	return do_get(&ctx, buf);
+	return do_ep(IC_GET_MSGS, API_VER,
+		     NULL, NULL, buf, cid, (char *)NULL);
 }
 
 /*
@@ -34,14 +29,8 @@ int mtd_ic_get_messages(const char *cid, char **buf)
  */
 int mtd_ic_get_end_of_year_est(const char *cid, char **buf)
 {
-	struct curl_ctx ctx = { 0 };
-	const char *params[] = { cid };
-
-	ctx.mtd_api_ver = API_VER;
-	ctx.endpoint = IC_GET_EOY_EST;
-	ctx.params = params;
-
-	return do_get(&ctx, buf);
+	return do_ep(IC_GET_EOY_EST, API_VER,
+		     NULL, NULL, buf, cid, (char *)NULL);
 }
 
 /*
@@ -50,14 +39,8 @@ int mtd_ic_get_end_of_year_est(const char *cid, char **buf)
  */
 int mtd_ic_get_allowances_deductions_reliefs(const char *cid, char **buf)
 {
-	struct curl_ctx ctx = { 0 };
-	const char *params[] = { cid };
-
-	ctx.mtd_api_ver = API_VER;
-	ctx.endpoint = IC_GET_ALLOWANCES_DEDUCT_RELIEFS;
-	ctx.params = params;
-
-	return do_get(&ctx, buf);
+	return do_ep(IC_GET_ALLOWANCES_DEDUCT_RELIEFS, API_VER,
+		     NULL, NULL, buf, cid, (char *)NULL);
 }
 
 /*
@@ -66,14 +49,8 @@ int mtd_ic_get_allowances_deductions_reliefs(const char *cid, char **buf)
  */
 int mtd_ic_get_taxable_income(const char *cid, char **buf)
 {
-	struct curl_ctx ctx = { 0 };
-	const char *params[] = { cid };
-
-	ctx.mtd_api_ver = API_VER;
-	ctx.endpoint = IC_GET_TAXABLE_INCOME;
-	ctx.params = params;
-
-	return do_get(&ctx, buf);
+	return do_ep(IC_GET_TAXABLE_INCOME, API_VER,
+		     NULL, NULL, buf, cid, (char *)NULL);
 }
 
 /*
@@ -82,14 +59,8 @@ int mtd_ic_get_taxable_income(const char *cid, char **buf)
  */
 int mtd_ic_get_income_tax_nics_calc(const char *cid, char **buf)
 {
-	struct curl_ctx ctx = { 0 };
-	const char *params[] = { cid };
-
-	ctx.mtd_api_ver = API_VER;
-	ctx.endpoint = IC_GET_INCOME_TAX_NICS_CALC;
-	ctx.params = params;
-
-	return do_get(&ctx, buf);
+	return do_ep(IC_GET_INCOME_TAX_NICS_CALC, API_VER,
+		     NULL, NULL, buf, cid, (char *)NULL);
 }
 
 /*
@@ -98,14 +69,8 @@ int mtd_ic_get_income_tax_nics_calc(const char *cid, char **buf)
  */
 int mtd_ic_get_calculation_meta(const char *cid, char **buf)
 {
-	struct curl_ctx ctx = { 0 };
-	const char *params[] = { cid };
-
-	ctx.mtd_api_ver = API_VER;
-	ctx.endpoint = IC_GET_CALCULATION_META;
-	ctx.params = params;
-
-	return do_get(&ctx, buf);
+	return do_ep(IC_GET_CALCULATION_META, API_VER,
+		     NULL, NULL, buf, cid, (char *)NULL);
 }
 
 /*
@@ -114,12 +79,8 @@ int mtd_ic_get_calculation_meta(const char *cid, char **buf)
  */
 int mtd_ic_trigger_calculation(const char *src_file, char **buf)
 {
-	struct curl_ctx ctx = { 0 };
-
-	ctx.mtd_api_ver = API_VER;
-	ctx.endpoint = IC_TRIGGER_CALCULATION;
-
-	return do_post(&ctx, src_file, NULL, buf);
+	return do_ep(IC_TRIGGER_CALCULATION, API_VER,
+		     src_file, NULL, buf, (char *)NULL);
 }
 
 /*
@@ -128,10 +89,6 @@ int mtd_ic_trigger_calculation(const char *src_file, char **buf)
  */
 int mtd_ic_list_calculations(char **buf)
 {
-	struct curl_ctx ctx = { 0 };
-
-	ctx.mtd_api_ver = API_VER;
-	ctx.endpoint = IC_LIST_CALCULATIONS;
-
-	return do_get(&ctx, buf);
+	return do_ep(IC_LIST_CALCULATIONS, API_VER,
+		     NULL, NULL, buf, (const char *)NULL);
 }
