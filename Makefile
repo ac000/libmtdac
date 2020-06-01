ac000/libmtdac

commit 85e41aec852b23be7d8d27da1fcfaef4b127b81b

src/Makefile: Get the full version
Use git describe --long --dirty to get the full version of the library,
e.g

    v0.1.0-30-g2f01ef7

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/Makefile
@@ -1,5 +1,7 @@
 LIBNAME = libmtdac
 
+GIT_VERSION  = \"$(shell git describe --long --dirty)\"
+
 SOVER	= 0
 VERSION = $(SOVER).1.0
 
@@ -11,7 +13,7 @@ CC	= gcc
 CFLAGS	= -Wall -Wextra -Wdeclaration-after-statement -Wvla \
 	  -g -O2 -Wp,-D_FORTIFY_SOURCE=2 --param=ssp-buffer-size=4 \
 	  -fPIC -fexceptions -fvisibility=hidden -I../include/libmtdac \
-	  -DLIBNAME=\"$(LIBNAME)\"
+	  -DLIBNAME=\"$(LIBNAME)\" -DGIT_VERSION=${GIT_VERSION}
 LDFLAGS = -fpic -shared -Wl,-z,now,-z,defs,-z,relro,--as-needed
 LIBS	= -lcurl -ljansson
 POSTCOMPILE = @mv -f $(DEPDIR)/$*.Td $(DEPDIR)/$*.d && touch $@

commit a926be30bf9c4f2eb152932db7b3f962101c9b35

Makefile: Fix rpm build
'make rpm' was broken by commit 85e41ae ("src/Makefile: Get the full
version"), due to when building the rpm the source is no longer under
git and there's no .git directory so 'git describe' fails.

Fix this with a bit of trickery.

When doing 'make rpm' we put the output of 'git describe | tail -c +2'
into a .version file in the repository root, this is also gitignored as
it's only used for building the rpm.

We then append this file into the tar archive created by 'git archive
...' we need to use the --transform option to tar so we can add in the
tar prefix path as created by 'git archive ...'.

In src/Makefile we adjust our command for getting the version string to
try 'cat ../.version' if the 'git describe' fails (which it will when
building the rpm).

Fixes: 85e41ae ("src/Makefile: Get the full version")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

.gitignore
@@ -2,3 +2,6 @@
 *~
 
 .gdb_history
+
+# Auto generated by 'make rpm'
+.version

Makefile
@@ -22,7 +22,9 @@ ifeq ($(wildcard ~/rpmbuild/),)
 	@false
 else
 	@version=$$(git describe | tail -c +2); echo "Building $${version}"; \
-		git archive --prefix=libmtdac-$${version%%-*}/ -o ~/rpmbuild/SOURCES/libmtdac-$${version%%-*}.tar HEAD
+		git archive --prefix=libmtdac-$${version%%-*}/ -o ~/rpmbuild/SOURCES/libmtdac-$${version%%-*}.tar HEAD; \
+		git describe | tail -c +2 > .version; \
+		tar -rf ~/rpmbuild/SOURCES/libmtdac-$${version%%-*}.tar --transform "s,^,libmtdac-$${version%%-*}/," .version
 	@rpmbuild -bb libmtdac.spec
 endif
 
@@ -30,3 +32,4 @@ endif
 clean:
 	@echo -e "Cleaning: libmtdac"
 	@$(MAKE) $(MAKE_OPTS) -C src/ clean
+	@rm -f .version

src/Makefile
@@ -1,6 +1,6 @@
 LIBNAME = libmtdac
 
-GIT_VERSION  = \"$(shell git describe --long --dirty)\"
+GIT_VERSION  = \"$(shell git describe --long --dirty 2> /dev/null || cat ../.version)\"
 
 SOVER	= 0
 VERSION = $(SOVER).9.0