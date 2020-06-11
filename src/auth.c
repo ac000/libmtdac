ac000/libmtdac

commit ffbe73d3c38064a1b22489948df469187594eeec

mtd, auth: Fix oauth.json
With commit 34b21f6 ("curler: Handle re-directs") the JSON responses are
wrapped in an array including the oauth JSON, which meant that
refresh_access_token() couldn't get the refresh token from oauth.json.

We don't really need the oauth.json to be wrapped in an array so when
writing out this file extract the core JSON from the array.

Fixes: 34b21f6 ("curler: Handle re-directs")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/auth.c
@@ -51,6 +51,7 @@ int refresh_access_token(void)
 	char *refresh_token = load_token("refresh_token", FT_AUTH);
 	char *client_id = load_token("client_id", FT_CONFIG);
 	char *client_secret = load_token("client_secret", FT_CONFIG);
+	json_t *array;
 	json_t *root;
 	json_t *result;
 	int err;
@@ -69,10 +70,11 @@ int refresh_access_token(void)
 	snprintf(path, sizeof(path), MTD_CONFIG_DIR_FMT, getenv("HOME"),
 		 "oauth.json");
 
-	root = json_loads(buf, 0, NULL);
+	array = json_loads(buf, 0, NULL);
+	root = json_array_get(array, 0);
 	result = json_object_get(root, "result");
 	json_dump_file(result, path, JSON_INDENT(4));
-	json_decref(root);
+	json_decref(array);
 	json_decref(result);
 
 out_free:

src/mtd.c
@@ -159,6 +159,7 @@ int mtd_init_auth(void)
 	char data[4096];
 	char path[PATH_MAX];
 	char *s;
+	json_t *array;
 	json_t *root;
 	json_t *result;
 	pid_t child_pid;
@@ -210,12 +211,13 @@ int mtd_init_auth(void)
 
 	printf("\n");
 	printf("Wrote oauth.json to %s\n\n", path);
-	root = json_loads(buf, 0, NULL);
+	array = json_loads(buf, 0, NULL);
+	root = json_array_get(array, 0);
 	result = json_object_get(root, "result");
 	json_dumpf(result, stdout, JSON_INDENT(4));
 	json_dump_file(result, path, JSON_INDENT(4));
 	printf("\n");
-	json_decref(root);
+	json_decref(array);
 	json_decref(result);
 
 out_free:

commit 3159c788aee2c3cf4ee73af145dedb26c4d987ce

auth: Fix an invalid memory read in oauther_refresh_access_token()
When running under valgrind I saw the following

==63768== Invalid read of size 8
==63768==    at 0x4887C09: json_decref (jansson.h:134)
==63768==    by 0x4887C09: oauther_refresh_access_token (auth.c:96)
==63768==    by 0x4889BDB: do_curl (curler.c:383)
==63768==    by 0x488A02D: do_get_delete (curler.c:480)
==63768==    by 0x488A02D: do_get (curler.c:496)
==63768==    by 0x48875E2: do_ep (endpoints.c:639)
==63768==    by 0x403783: do_api_func (mtd-cli.c:148)
==63768==    by 0x4033A1: do_mtd_api (mtd-cli.c:181)
==63768==    by 0x4033A1: dispatcher (mtd-cli.c:255)
==63768==    by 0x4033A1: main (mtd-cli.c:300)
==63768==  Address 0x662a328 is 8 bytes inside a block of size 72 free'd
==63768==    at 0x483B9F5: free (vg_replace_malloc.c:538)
==63768==    by 0x4875BFB: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4875C9C: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x487B7A8: json_delete (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x487B785: json_delete (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4887C87: json_decref (jansson.h:135)
==63768==    by 0x4887C87: json_decref (jansson.h:132)
==63768==    by 0x4887C87: oauther_refresh_access_token (auth.c:95)
==63768==    by 0x4889BDB: do_curl (curler.c:383)
==63768==    by 0x488A02D: do_get_delete (curler.c:480)
==63768==    by 0x488A02D: do_get (curler.c:496)
==63768==    by 0x48875E2: do_ep (endpoints.c:639)
==63768==    by 0x403783: do_api_func (mtd-cli.c:148)
==63768==    by 0x4033A1: do_mtd_api (mtd-cli.c:181)
==63768==    by 0x4033A1: dispatcher (mtd-cli.c:255)
==63768==    by 0x4033A1: main (mtd-cli.c:300)
==63768==  Block was alloc'd at
==63768==    at 0x483A809: malloc (vg_replace_malloc.c:307)
==63768==    by 0x487AC0F: json_object (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x48775E6: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4877681: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4877816: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x48779C4: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4877B4A: json_loads (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4887BB8: oauther_refresh_access_token (auth.c:91)
==63768==    by 0x4889BDB: do_curl (curler.c:383)
==63768==    by 0x488A02D: do_get_delete (curler.c:480)
==63768==    by 0x488A02D: do_get (curler.c:496)
==63768==    by 0x48875E2: do_ep (endpoints.c:639)
==63768==    by 0x403783: do_api_func (mtd-cli.c:148)
==63768==
==63768== Invalid read of size 8
==63768==    at 0x4887C13: json_decref (jansson.h:134)
==63768==    by 0x4887C13: json_decref (jansson.h:132)
==63768==    by 0x4887C13: oauther_refresh_access_token (auth.c:96)
==63768==    by 0x4889BDB: do_curl (curler.c:383)
==63768==    by 0x488A02D: do_get_delete (curler.c:480)
==63768==    by 0x488A02D: do_get (curler.c:496)
==63768==    by 0x48875E2: do_ep (endpoints.c:639)
==63768==    by 0x403783: do_api_func (mtd-cli.c:148)
==63768==    by 0x4033A1: do_mtd_api (mtd-cli.c:181)
==63768==    by 0x4033A1: dispatcher (mtd-cli.c:255)
==63768==    by 0x4033A1: main (mtd-cli.c:300)
==63768==  Address 0x662a328 is 8 bytes inside a block of size 72 free'd
==63768==    at 0x483B9F5: free (vg_replace_malloc.c:538)
==63768==    by 0x4875BFB: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4875C9C: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x487B7A8: json_delete (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x487B785: json_delete (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4887C87: json_decref (jansson.h:135)
==63768==    by 0x4887C87: json_decref (jansson.h:132)
==63768==    by 0x4887C87: oauther_refresh_access_token (auth.c:95)
==63768==    by 0x4889BDB: do_curl (curler.c:383)
==63768==    by 0x488A02D: do_get_delete (curler.c:480)
==63768==    by 0x488A02D: do_get (curler.c:496)
==63768==    by 0x48875E2: do_ep (endpoints.c:639)
==63768==    by 0x403783: do_api_func (mtd-cli.c:148)
==63768==    by 0x4033A1: do_mtd_api (mtd-cli.c:181)
==63768==    by 0x4033A1: dispatcher (mtd-cli.c:255)
==63768==    by 0x4033A1: main (mtd-cli.c:300)
==63768==  Block was alloc'd at
==63768==    at 0x483A809: malloc (vg_replace_malloc.c:307)
==63768==    by 0x487AC0F: json_object (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x48775E6: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4877681: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4877816: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x48779C4: ??? (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4877B4A: json_loads (in /usr/lib64/libjansson.so.4.11.1)
==63768==    by 0x4887BB8: oauther_refresh_access_token (auth.c:91)
==63768==    by 0x4889BDB: do_curl (curler.c:383)
==63768==    by 0x488A02D: do_get_delete (curler.c:480)
==63768==    by 0x488A02D: do_get (curler.c:496)
==63768==    by 0x48875E2: do_ep (endpoints.c:639)
==63768==    by 0x403783: do_api_func (mtd-cli.c:148)
==63768==

This only happened when we were refreshing the access token.

Turns out this has actually been there since day 1 (and while I've run
it many times under valgrind, I guess I've never run it when it was
refreshing the access token).

The problem is simply one too many calls to json_decref() in
oauther_refresh_access_token(), we had

    json_decref(array);
    json_decref(result);

Now that second one shouldn't be there as the first one will free all
the memory, result ultimately comes out of array.

This issue was (seemingly) introduced by commit ffbe73d ("mtd, auth: Fix

src/auth.c
@@ -93,7 +93,6 @@ int oauther_refresh_access_token(void)
 	result = json_object_get(root, "result");
 	json_dump_file(result, path, JSON_INDENT(4));
 	json_decref(array);
-	json_decref(result);
 
 out_free:
 	free(buf);