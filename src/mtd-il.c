ac000/libmtdac

commit 08eb2cfbada82745c498041bb79ae1c2871e09c4

Totally re-work how the source JSON payload is specified
This re-works how the JSON payload data for POST/PUT endpoints is
specified.

Originally this was simply done by passing a filename into the endpoint
API functions.

That was changed in commit 30765cb ("Allow to use a buffer for the JSON
payload rather than a file") to allow alternatively specifying a buffer
that contained the JSON, this was done using the mtd_ctx thread local
storage infrastructure.

This change basically undoes that and then goes much further.

Rather than passing a filename (or using the TLS buffer) into the
endpoint API functions. This has been replaced with a new mtd_dsrc_ctx
structure.

This can be used to specify a filename or a buffer, but also a stdio
FILE * file pointer or a file descriptor of already open files. Both of
which are useful mechanisms.

The mtd_dsrc_ctx structure holds a union of the above methods and then
you specify which method you are actually using in mtd_dsrc_ctx. This
new API looks like (defined in include/libmtdac/mtd.h)

    enum mtd_data_src_type {
        MTD_DATA_SRC_FILE = 0,
        MTD_DATA_SRC_BUF,
        MTD_DATA_SRC_FP,
        MTD_DATA_SRC_FD,
    };

    typedef union mtd_data_src {
        const void *buf;
        const char *file;
        FILE *fp;
        int fd;
    } mtd_data_src_t;

    struct mtd_dsrc_ctx {
        mtd_data_src_t data_src;
        size_t data_len;

        enum mtd_data_src_type src_type;
    };

The buf and file pointers are const as that is what you will often have
and thus avoids needing casting.

This will also allow for additional methods to be added, e.g it should
be possible to add the ability to specify a callback function to get the
data.

This is documented in the API.md and a new mtd_dsrc_ctx.3 man page.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd-il.c
@@ -17,21 +17,21 @@
  * [PUT ]
  * /individuals/losses/{nino}/loss-claims/order
  */
-int mtd_il_lc_update_loss_order(const char *src_file, char **buf)
+int mtd_il_lc_update_loss_order(const struct mtd_dsrc_ctx *dsctx, char **buf)
 {
 	return do_ep(IL_LC_UPDATE_LOSS_ORDER, API_VER,
-		     src_file, NULL, buf, (char *)NULL);
+		     dsctx, buf, (char *)NULL);
 }
 
 /*
  * [POST]
  * /individuals/losses/{nino}/loss-claims/{claimId}/change-type-of-claim
  */
-int mtd_il_lc_update_loss_type(const char *src_file, const char *cid,
-			       char **buf)
+int mtd_il_lc_update_loss_type(const struct mtd_dsrc_ctx *dsctx,
+			       const char *cid, char **buf)
 {
 	return do_ep(IL_LC_UPDATE_LOSS_TYPE, API_VER,
-		     src_file, NULL, buf, cid, (char *)NULL);
+		     dsctx, buf, cid, (char *)NULL);
 }
 
 /*
@@ -58,10 +58,10 @@ int mtd_il_lc_get_loss(const char *cid, char **buf)
  * [POST]
  * /individuals/losses/{nino}/loss-claims
  */
-int mtd_il_lc_create_loss(const char *src_file, char **buf)
+int mtd_il_lc_create_loss(const struct mtd_dsrc_ctx *dsctx, char **buf)
 {
 	return do_ep(IL_LC_CREATE_LOSS, API_VER,
-		     src_file, NULL, buf, (char *)NULL);
+		     dsctx, buf, (char *)NULL);
 }
 
 /*
@@ -84,11 +84,11 @@ int mtd_il_lc_list_loses(const char *query_string, char **buf)
  * [POST]
  * /individuals/losses/{nino}/brought-forward-losses/{lossId}/change-loss-amount
  */
-int mtd_il_bf_update_loss_amnt(const char *src_file, const char *lid,
-			       char **buf)
+int mtd_il_bf_update_loss_amnt(const struct mtd_dsrc_ctx *dsctx,
+			       const char *lid, char **buf)
 {
 	return do_ep(IL_BF_UPDATE_LOSS_AMNT, API_VER,
-		     src_file, NULL, buf, lid, (char *)NULL);
+		     dsctx, buf, lid, (char *)NULL);
 }
 
 /*
@@ -115,10 +115,10 @@ int mtd_il_bf_get_loss(const char *lid, char **buf)
  * [POST]
  * /individuals/losses/{nino}/brought-forward-losses
  */
-int mtd_il_bf_create_loss(const char *src_file, char **buf)
+int mtd_il_bf_create_loss(const struct mtd_dsrc_ctx *dsctx, char **buf)
 {
 	return do_ep(IL_BF_CREATE_LOSS, API_VER,
-		     src_file, NULL, buf, (char *)NULL);
+		     dsctx, buf, (char *)NULL);
 }
 
 /*

include/libmtdac/mtd-bsas.h
@@ -17,17 +17,18 @@ extern "C" {
 
 #pragma GCC visibility push(default)
 
-extern int mtd_bsas_pb_update_summary_adjustments(const char *src_file,
+extern int mtd_bsas_pb_update_summary_adjustments(const struct mtd_dsrc_ctx *dsctx,
 						  const char *bid, char **buf);
 extern int mtd_bsas_pb_list_summary_adjustments(const char *bid, char **buf);
 extern int mtd_bsas_pb_get_summary(const char *bid, const char *query_string,
 				   char **buf);
-extern int mtd_bsas_se_update_summary_adjustments(const char *src_file,
+extern int mtd_bsas_se_update_summary_adjustments(const struct mtd_dsrc_ctx *dsctx,
 						  const char *bid, char **buf);
 extern int mtd_bsas_se_list_summary_adjustments(const char *bid, char **buf);
 extern int mtd_bsas_se_get_summary(const char *bid, const char *query_string,
 				   char **buf);
-extern int mtd_bsas_trigger_summary(const char *src_file, char **buf);
+extern int mtd_bsas_trigger_summary(const struct mtd_dsrc_ctx *dsctx,
+				    char **buf);
 extern int mtd_bsas_list_summaries(const char *query_string, char **buf);
 
 #pragma GCC visibility pop

include/libmtdac/mtd-ic.h
@@ -24,7 +24,8 @@ extern int mtd_ic_get_allowances_deductions_reliefs(const char *cid,
 extern int mtd_ic_get_taxable_income(const char *cid, char **buf);
 extern int mtd_ic_get_income_tax_nics_calc(const char *cid, char **buf);
 extern int mtd_ic_get_calculation_meta(const char *cid, char **buf);
-extern int mtd_ic_trigger_calculation(const char *src_file, char **buf);
+extern int mtd_ic_trigger_calculation(const struct mtd_dsrc_ctx *dsctx,
+				      char **buf);
 extern int mtd_ic_list_calculations(const char *query_string, char **buf);
 
 #pragma GCC visibility pop

include/libmtdac/mtd-il.h
@@ -17,18 +17,19 @@ extern "C" {
 
 #pragma GCC visibility push(default)
 
-extern int mtd_il_lc_update_loss_order(const char *src_file, char **buf);
-extern int mtd_il_lc_update_loss_type(const char *src_file, const char *cid,
-				      char **buf);
+extern int mtd_il_lc_update_loss_order(const struct mtd_dsrc_ctx *dsctx,
+				       char **buf);
+extern int mtd_il_lc_update_loss_type(const struct mtd_dsrc_ctx *dsctx,
+				      const char *cid, char **buf);
 extern int mtd_il_lc_delete_loss(const char *cid, char **buf);
 extern int mtd_il_lc_get_loss(const char *cid, char **buf);
-extern int mtd_il_lc_create_loss(const char *src_file, char **buf);
+extern int mtd_il_lc_create_loss(const struct mtd_dsrc_ctx *dsctx, char **buf);
 extern int mtd_il_lc_list_loses(const char *query_string, char **buf);
-extern int mtd_il_bf_update_loss_amnt(const char *src_file, const char *lid,
-				      char **buf);
+extern int mtd_il_bf_update_loss_amnt(const struct mtd_dsrc_ctx *dsctx,
+				      const char *lid, char **buf);
 extern int mtd_il_bf_delete_loss(const char *lid, char **buf);
 extern int mtd_il_bf_get_loss(const char *lid, char **buf);
-extern int mtd_il_bf_create_loss(const char *src_file, char **buf);
+extern int mtd_il_bf_create_loss(const struct mtd_dsrc_ctx *dsctx, char **buf);
 extern int mtd_il_bf_list_loses(const char *query_string, char **buf);
 
 #pragma GCC visibility pop

include/libmtdac/mtd-sa.h
@@ -18,84 +18,89 @@ extern "C" {
 #pragma GCC visibility push(default)
 
 extern int mtd_sa_cr_list_obligations(const char *query_string, char **buf);
-extern int mtd_sa_cr_crystallise(const char *src_file, const char *tax_year,
-				 char **buf);
+extern int mtd_sa_cr_crystallise(const struct mtd_dsrc_ctx *dsctx,
+				 const char *tax_year, char **buf);
 extern int mtd_sa_cr_intent_to_crystallise(const char *tax_year, char **buf);
 
 extern int mtd_sa_tc_get_validation_msgs(const char *cid, char **buf);
 extern int mtd_sa_tc_get_calculation(const char *cid, char **buf);
-extern int mtd_sa_tc_calculate(const char *src_file, char **buf);
+extern int mtd_sa_tc_calculate(const struct mtd_dsrc_ctx *dsctx, char **buf);
 
-extern int mtd_sa_cg_update_charitable_giving(const char *src_file,
+extern int mtd_sa_cg_update_charitable_giving(const struct mtd_dsrc_ctx *dsctx,
 					      const char *tax_year,
 					      char **buf);
 extern int mtd_sa_cg_get_charitable_giving(const char *tax_year, char **buf);
 
-extern int mtd_sa_sa_update_annual_summary(const char *src_file,
+extern int mtd_sa_sa_update_annual_summary(const struct mtd_dsrc_ctx *dsctx,
 					   const char *said,
 					   const char *tax_year, char **buf);
 extern int mtd_sa_sa_get_annual_summary(const char *said, const char *tax_year,
 					char **buf);
 extern int mtd_sa_sa_get_account(const char *said, char **buf);
-extern int mtd_sa_sa_create_account(const char *src_file, char **buf);
+extern int mtd_sa_sa_create_account(const struct mtd_dsrc_ctx *dsctx,
+				    char **buf);
 extern int mtd_sa_sa_list_accounts(char **buf);
 
-extern int mtd_sa_di_update_annual_summary(const char *src_file,
+extern int mtd_sa_di_update_annual_summary(const struct mtd_dsrc_ctx *dsctx,
 					   const char *tax_year, char **buf);
 extern int mtd_sa_di_get_annual_summary(const char *tax_year, char **buf);
 
 extern int mtd_sa_pb_get_end_of_period_statement(const char *query_string,
 						 char **buf);
-extern int mtd_sa_pb_submit_end_of_period_statement(const char *src_file,
+extern int mtd_sa_pb_submit_end_of_period_statement(const struct mtd_dsrc_ctx *dsctx,
 						    const char *start,
 						    const char *end,
 						    char **buf);
-extern int mtd_sa_pb_update_fhl_annual_summary(const char *src_file,
+extern int mtd_sa_pb_update_fhl_annual_summary(const struct mtd_dsrc_ctx *dsctx,
 					       const char *tax_year,
 					       char **buf);
 extern int mtd_sa_pb_get_fhl_annual_summary(const char *tax_year, char **buf);
-extern int mtd_sa_pb_update_fhl_period(const char *src_file, const char *pid,
-				       char **buf);
+extern int mtd_sa_pb_update_fhl_period(const struct mtd_dsrc_ctx *dsctx,
+				       const char *pid, char **buf);
 extern int mtd_sa_pb_get_fhl_period(const char *pid, char **buf);
-extern int mtd_sa_pb_create_fhl_period(const char *src_file, char **buf);
+extern int mtd_sa_pb_create_fhl_period(const struct mtd_dsrc_ctx *dsctx,
+				       char **buf);
 extern int mtd_sa_pb_list_fhl_periods(char **buf);
-extern int mtd_sa_pb_update_non_fhl_annual_summary(const char *src_file,
+extern int mtd_sa_pb_update_non_fhl_annual_summary(const struct mtd_dsrc_ctx *dsctx,
 						   const char *tax_year,
 						   char **buf);
 extern int mtd_sa_pb_get_non_fhl_annual_summary(const char *tax_year,
 						char **buf);
-extern int mtd_sa_pb_update_non_fhl_period(const char *src_file,
+extern int mtd_sa_pb_update_non_fhl_period(const struct mtd_dsrc_ctx *dsctx,
 					   const char *pid, char **buf);
 extern int mtd_sa_pb_get_non_fhl_period(const char *pid, char **buf);
-extern int mtd_sa_pb_create_non_fhl_period(const char *src_file, char **buf);
+extern int mtd_sa_pb_create_non_fhl_period(const struct mtd_dsrc_ctx *dsctx, char **buf);
 extern int mtd_sa_pb_list_non_fhl_periods(char **buf);
 extern int mtd_sa_pb_list_obligations(char **buf);
-extern int mtd_sa_pb_create_property(const char *src_file, char **buf);
+extern int mtd_sa_pb_create_property(const struct mtd_dsrc_ctx *dsctx,
+				     char **buf);
 extern int mtd_sa_pb_get_property(char **buf);
 
 extern int mtd_sa_se_get_end_of_period_statement(const char *seid,
 						 const char *query_string,
 						 char **buf);
-extern int mtd_sa_se_submit_end_of_period_statement(const char *src_file,
+extern int mtd_sa_se_submit_end_of_period_statement(const struct mtd_dsrc_ctx *dsctx,
 						    const char *seid,
 						    const char *start,
 						    const char *end,
 						    char **buf);
-extern int mtd_sa_se_update_annual_summary(const char *src_file,
+extern int mtd_sa_se_update_annual_summary(const struct mtd_dsrc_ctx *dsctx,
 					   const char *seid,
 					   const char *tax_year, char **buf);
 extern int mtd_sa_se_get_annual_summary(const char *seid, const char *tax_year,
 					char **buf);
-extern int mtd_sa_se_update_period(const char *src_file, const char *seid,
-				   const char *period_id, char **buf);
+extern int mtd_sa_se_update_period(const struct mtd_dsrc_ctx *dsctx,
+				   const char *seid, const char *period_id,
+				   char **buf);
 extern int mtd_sa_se_get_period(const char *seid, const char *period_id,
 				char **buf);
-extern int mtd_sa_se_create_period(const char *src_file, const char *seid,
-				   char **buf);
+extern int mtd_sa_se_create_period(const struct mtd_dsrc_ctx *dsctx,
+				   const char *seid, char **buf);
 extern int mtd_sa_se_list_periods(const char *seid, char **buf);
 extern int mtd_sa_se_list_obligations(const char *seid, char **buf);
 extern int mtd_sa_se_get_employment(const char *seid, char **buf);
-extern int mtd_sa_se_create_employment(const char *src_file, char **buf);
+extern int mtd_sa_se_create_employment(const struct mtd_dsrc_ctx *dsctx,
+				       char **buf);
 extern int mtd_sa_se_list_employments(char **buf);
 
 #pragma GCC visibility pop

include/libmtdac/mtd-test-cu.h
@@ -18,9 +18,12 @@ extern "C" {
 #pragma GCC visibility push(default)
 
 extern int mtd_test_cu_list_services(char **buf);
-extern int mtd_test_cu_create_agent(const char *src_file, char **buf);
-extern int mtd_test_cu_create_organisation(const char *src_file, char **buf);
-extern int mtd_test_cu_create_individual(const char *src_file, char **buf);
+extern int mtd_test_cu_create_agent(const struct mtd_dsrc_ctx *dsctx,
+				    char **buf);
+extern int mtd_test_cu_create_organisation(const struct mtd_dsrc_ctx *dsctx,
+					   char **buf);
+extern int mtd_test_cu_create_individual(const struct mtd_dsrc_ctx *dsctx,
+					 char **buf);
 
 #pragma GCC visibility pop
 

include/libmtdac/mtd-test-ni.h
@@ -17,7 +17,7 @@ extern "C" {
 
 #pragma GCC visibility push(default)
 
-extern int mtd_test_ni_create_annual_summary(const char *src_file,
+extern int mtd_test_ni_create_annual_summary(const struct mtd_dsrc_ctx *dsctx,
 					     const char *utr,
 					     const char *tax_year, char **buf);
 

include/libmtdac/mtd.h
@@ -9,6 +9,8 @@
 #ifndef _MTD_H_
 #define _MTD_H_
 
+#include <stdio.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -63,10 +65,29 @@ enum mtd_error {
 	MTD_ERR_UNKNOWN_FLAGS,
 };
 
+enum mtd_data_src_type {
+	MTD_DATA_SRC_FILE = 0,
+	MTD_DATA_SRC_BUF,
+	MTD_DATA_SRC_FP,
+	MTD_DATA_SRC_FD,
+};
+
+typedef union mtd_data_src {
+	const void *buf;
+	const char *file;
+	FILE *fp;
+	int fd;
+} mtd_data_src_t;
+
+struct mtd_dsrc_ctx {
+	mtd_data_src_t data_src;
+	size_t data_len;
+
+	enum mtd_data_src_type src_type;
+};
+
 #pragma GCC visibility push(default)
 
-extern void mtd_unset_src_data(void);
-extern void mtd_set_src_data(const void *buf, size_t len);
 extern void mtd_hdrs_add(const char * const hdrs[]);
 extern void mtd_hdrs_reset(void);
 extern void mtd_global_init(void);

commit 7d81b8e5b4a2b7cef12f6a3533ff6aed489d61cd

mtd-il: Fix some of the endpoints
Some of the these endpoints got broken by commit 08eb2cf ("Totally
re-work how the source JSON payload is specified") where the do_ep()
function prototype got changed, basically the src_file & data arguments
were replaced with a single struct mtd_dsrc_ctx *dsctx argument.

It seems for some reason in this file I only updated endpoints that had
taken a src_file argument and correctly changed them from something like

    ...
    src_file, NULL, buf, (char *)NULL);

to

    ...
    dsctx, buf, (char *)NULL);

however others were left looking something like

    ...
    NULL, NULL, buf, cid, (char *)NULL);

notice the extra NULL in there pushing all the arguments along one and
in fact as soon as you try using one of these endpoints you get a
segfault due to do_ep() getting NULL for buf, rather than buf...

Fixes: 08eb2cf ("Totally re-work how the source JSON payload is specified")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd-il.c
@@ -3,7 +3,7 @@
 /*
  * mtd-il.c - Make Tax Digital - Individual Loses API
  *
- * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 2020 - 2021	Andrew Clayton <andrew@digital-domain.net>
  */
 
 #include <stddef.h>
@@ -46,7 +46,7 @@ int mtd_il_lc_update_loss_type(const struct mtd_dsrc_ctx *dsctx,
 int mtd_il_lc_delete_loss(const char *cid, char **buf)
 {
 	return do_ep(IL_LC_DELETE_LOSS, API_VER,
-		     NULL, NULL, buf, cid, (char *)NULL);
+		     NULL, buf, cid, (char *)NULL);
 }
 
 /*
@@ -56,7 +56,7 @@ int mtd_il_lc_delete_loss(const char *cid, char **buf)
 int mtd_il_lc_get_loss(const char *cid, char **buf)
 {
 	return do_ep(IL_LC_GET_LOSS, API_VER,
-		     NULL, NULL, buf, cid, (char *)NULL);
+		     NULL, buf, cid, (char *)NULL);
 }
 
 /*
@@ -82,7 +82,7 @@ int mtd_il_lc_create_loss(const struct mtd_dsrc_ctx *dsctx, char **buf)
 int mtd_il_lc_list_loses(const char *query_string, char **buf)
 {
 	return do_ep(IL_LC_LIST_LOSES, API_VER,
-		     NULL, NULL, buf, query_string, (char *)NULL);
+		     NULL, buf, query_string, (char *)NULL);
 }
 
 /*
@@ -103,7 +103,7 @@ int mtd_il_bf_update_loss_amnt(const struct mtd_dsrc_ctx *dsctx,
 int mtd_il_bf_delete_loss(const char *lid, char **buf)
 {
 	return do_ep(IL_BF_DELETE_LOSS, API_VER,
-		     NULL, NULL, buf, lid, (char *)NULL);
+		     NULL, buf, lid, (char *)NULL);
 }
 
 /*
@@ -113,7 +113,7 @@ int mtd_il_bf_delete_loss(const char *lid, char **buf)
 int mtd_il_bf_get_loss(const char *lid, char **buf)
 {
 	return do_ep(IL_BF_GET_LOSS, API_VER,
-		     NULL, NULL, buf, lid, (char *)NULL);
+		     NULL, buf, lid, (char *)NULL);
 }
 
 /*
@@ -139,5 +139,5 @@ int mtd_il_bf_create_loss(const struct mtd_dsrc_ctx *dsctx, char **buf)
 int mtd_il_bf_list_loses(const char *query_string, char **buf)
 {
 	return do_ep(IL_BF_LIST_LOSES, API_VER,
-		     NULL, NULL, buf, query_string, (char *)NULL);
+		     NULL, buf, query_string, (char *)NULL);
 }