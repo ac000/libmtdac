ac000/libmtdac

commit 0860febce17d35e71f10139bb19223b63675c85a

mtd: Make the config per client
Rather than the various json config files being stored under
~/.config/libmtdac, have the client specify where to store them.

This is done via a new config_dir member of struct mtd_cfg

This allows for per client configurations, in fact a client may have
multiple configurations to support multiple users for example.

We still create a libmtdac/ directory, then under that will be
{prod,test}-api directories. These directories are created as mode 0700.

Of course, multiple clients can still share the config by simply
symlinking the libmtdac/ directory between them.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd.c
@@ -3,7 +3,7 @@
 /*
  * mtd.c - Make Tax Digital
  *
- * Copyright (C) 2020		Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 2020 - 2021	Andrew Clayton <andrew@digital-domain.net>
  */
 
 #define _GNU_SOURCE
@@ -33,8 +33,6 @@
 
 #define TEST_API_URL		"https://test-api.service.hmrc.gov.uk"
 #define PROD_API_URL		"https://api.service.hmrc.gov.uk"
-
-#define MTD_CONFIG_DIR_FMT	".config/libmtdac/%s"
 
 static const struct _mtd_err_map {
 	const enum mtd_error err;
@@ -68,6 +66,14 @@ static const struct _mtd_err_map {
 	[MTD_ERR_LIB_TOO_OLD] = {
 		.estr	= "MTD_ERR_LIB_TOO_OLD",
 		.str	= "Library version too old"
+	},
+	[MTD_ERR_CONFIG_DIR_UNSPEC] {
+		.estr	= "MTD_ERR_CONFIG_DIR_UNSPEC",
+		.str	= "Config directory not specified"
+	},
+	[MTD_ERR_CONFIG_DIR_INVALID] {
+		.estr	= "MTD_ERR_CONFIG_DIR_INVALID",
+		.str	= "Config directory path is invalid"
 	},
 
 	/* keep this last */
@@ -196,7 +202,7 @@ static int mkdir_p(int dirfd, const char *path, mode_t mode)
 {
 	char *dir;
 	char *ptr;
-	char mdir[4096] = "\0";
+	char mdir[PATH_MAX] = "\0";
 	int ret = 0;
 
 	if (strlen(path) >= sizeof(mdir)) {
@@ -210,47 +216,47 @@ static int mkdir_p(int dirfd, const char *path, mode_t mode)
 	dir = strdup(path);
 	ptr = dir;
 	for (;;) {
+		struct stat sb;
 		char *token;
+		int err;
 
 		token = strsep(&dir, "/");
 		if (!token)
 			break;
 
 		strcat(mdir, token);
+		strcat(mdir, "/");
+
+		err = fstatat(dirfd, mdir, &sb, 0);
+		if (!err)
+			continue;
+
 		ret = mkdirat(dirfd, mdir, mode);
 		if (ret == -1 && errno != EEXIST) {
 			ret = -1;
 			break;
 		}
-		strcat(mdir, "/");
 	}
 	free(ptr);
 
 	return ret;
 }
 
-static int check_config_dir(bool is_production)
+static int check_config_dir(const char *config_dir, bool is_production)
 {
-	char path[PATH_MAX];
 	char errbuf[129];
-	char *home_dir;
 	char *cfg_dir;
 	struct stat sb;
 	int dfd;
 	int err;
 	int ret = MTD_ERR_NONE;
 
-	home_dir = getenv("HOME");
-	if (!home_dir)
-		return -MTD_ERR_OS;
-
-	dfd = open(home_dir, O_PATH|O_DIRECTORY|O_CLOEXEC);
+	dfd = open(config_dir, O_PATH|O_DIRECTORY|O_CLOEXEC);
 	if (dfd == -1)
-		return -MTD_ERR_OS;
+		return -MTD_ERR_CONFIG_DIR_INVALID;
 
-	snprintf(path, sizeof(path), MTD_CONFIG_DIR_FMT,
-		 is_production ? "prod-api" : "test-api");
-	err = asprintf(&cfg_dir, "%s/%s", home_dir, path);
+	err = asprintf(&cfg_dir, "%s/libmtdac/%s", config_dir,
+		       is_production ? "prod-api" : "test-api");
 	if (err == -1) {
 		ret = -MTD_ERR_OS;
 		goto out_close;
@@ -258,16 +264,20 @@ static int check_config_dir(bool is_production)
 
 	mtd_ctx.config_dir = cfg_dir;
 
-	err = fstatat(dfd, path, &sb, 0);
+	/*
+	 * The chances are the config directory exists and we
+	 * can short cut out.
+	 */
+	err = fstatat(dfd, cfg_dir, &sb, 0);
 	if (!err)
 		goto out_close;
 
-	err = mkdir_p(dfd, path, 0777);
+	err = mkdir_p(dfd, cfg_dir, 0700);
 	if (!err)
 		goto out_close;
 
-	err = -MTD_ERR_OS;
-	logger(MTD_LOG_ERR, "mkdirat %s/%s: %s\n", getenv("HOME"), path,
+	ret = -MTD_ERR_OS;
+	logger(MTD_LOG_ERR, "mkdirat %s: %s\n", cfg_dir,
 	       strerror_r(errno, errbuf, sizeof(errbuf)));
 
 out_close:
@@ -294,7 +304,10 @@ int mtd_init(unsigned int flags, const struct mtd_cfg *cfg)
 		return -MTD_ERR_UNKNOWN_FLAGS;
 	mtd_ctx.opts = flags;
 
-	err = check_config_dir(flags & MTD_OPT_PRODUCTION_API);
+	if (!cfg->config_dir)
+		return -MTD_ERR_CONFIG_DIR_UNSPEC;
+	err = check_config_dir(cfg->config_dir,
+			       flags & MTD_OPT_PRODUCTION_API);
 	if (err)
 		return err;
 

include/libmtdac/mtd.h
@@ -64,6 +64,8 @@ enum mtd_error {
 	MTD_ERR_NEEDS_AUTHORISATION,
 	MTD_ERR_UNKNOWN_FLAGS,
 	MTD_ERR_LIB_TOO_OLD,
+	MTD_ERR_CONFIG_DIR_UNSPEC,
+	MTD_ERR_CONFIG_DIR_INVALID,
 
 	/* keep this last */
 	MTD_ERR_INVALID_ERROR
@@ -143,6 +145,8 @@ struct mtd_fph_ops {
 struct mtd_cfg {
 	const struct mtd_fph_ops *fph_ops;
 	const char * const *extra_hdrs;
+
+	const char *config_dir;
 };
 
 #pragma GCC visibility push(default)

man/man3/libmtdac_mtd.h.3
@@ -88,6 +88,8 @@ enum mtd_error {
         \fBMTD_ERR_NEEDS_AUTHORISATION\fP,
         \fBMTD_ERR_UNKNOWN_FLAGS\fP,
         \fBMTD_ERR_LIB_TOO_OLD\fP,
+        \fBMTD_ERR_CONFIG_DIR_UNSPEC\fP,
+        \fBMTD_ERR_CONFIG_DIR_INVALID\fP,
         \fBMTD_ERR_INVALID_ERROR\fP
 .br
 };
@@ -221,9 +223,16 @@ declare a \fBstruct mtd_fph_ops\fP as above and set it in \fBstruct mtd_cfg\fP.
 Also you can set extra HTTP headers to be sent, \fBextra_hdrs\fP should point
 to a NULL terminated array of character pointers.
 
+Finally, you \fBmust\fP specify the directory that libmtdac will use for its
+configuration data via \fBconfig_data\fP.
+
+Typically this would be something like \fB${HOME}/.config/${APP_NAME}\fP.
+
 struct mtd_cfg {
         const struct mtd_fph_ops *fph_ops;
         const char * const       *extra_hdrs;
+
+        const char               *config_dir;
 .br
 };
 
@@ -231,9 +240,15 @@ struct mtd_cfg {
 
 .EX
 const char *hdrs[2] = { NULL };
-const struct mtd_fph_ops fph_ops = { .fph_user = my_user,
-                                     .fph_version = my_ver };
-const struct mtd_cfg cfg = { .fph_ops = &fph_ops, .extra_hdrs = hdrs };
+const struct mtd_fph_ops fph_ops = {
+        .fph_user = my_user,
+        .fph_version = my_ver
+};
+const struct mtd_cfg cfg = {
+        .fph_ops = &fph_ops,
+        .extra_hdrs = hdrs,
+        .config_dir = "/home/foo/.config/mtd-cli"
+};
 
 hdrs[0] = getenv("MTD_CLI_HDRS");
 err = mtd_init(flags, &cfg);
@@ -244,7 +259,11 @@ You can also use the MTD_FPH_SET_FUNC() macro, e.g
 .EX
 const char *hdrs[2] = { NULL };
 struct mtd_fph_ops fph_ops = { NULL };
-const struct mtd_cfg cfg = { .fph_ops = &fph_ops, .extra_hdrs = hdrs };
+const struct mtd_cfg cfg = {
+        .fph_ops = &fph_ops,
+        .extra_hdrs = hdrs,
+        .config_dir = "/home/foo/.config/mtd-cli"
+};
 
 MTD_FPH_SET_FUNC(fph_ops, MTD_FPH_CLI_USER_ID, my_user);
 MTD_FPH_SET_FUNC(fph_ops, MTD_FPH_VEN_VERSION, my_ver);

man/man3/mtd_init.3
@@ -1,4 +1,4 @@
-.TH MTD_INIT 3 "January 7, 2021" "0.17.0" "libmtdac"
+.TH MTD_INIT 3 "January 28, 2021" "0.17.0" "libmtdac"
 
 .SH NAME
 
@@ -83,14 +83,16 @@ However at some point, it will become mandatory to send them for all endpoints.
 
 .PP
 
-\fIcfg\fP is an optional \fIstruct mtd_cfg\fP that can be used to
-override/provide various bits of library configuration.
+\fIcfg\fP is a \fIstruct mtd_cfg\fP that is used to override/provide various
+bits of library configuration.
 
 .RS 4
 .EX
 struct mtd_cfg {
         const struct mtd_fph_ops *fph_ops;
         const char * const       *extra_hdrs;
+
+        const char               *config_dir;
 };
 .EE
 .RE
@@ -164,9 +166,15 @@ One or more of the supplied flags weren't recognised
 
 unsigned int flags = MTD_OPT_GLOBAL_INIT | MTD_OPT_LOG_DEBUG;
 const char *hdrs[2] = { NULL };
-struct mtd_fph_ops fph_ops = { .fph_user = my_user,
-                               .fph_version = my_ver };
-struct mtd_cfg cfg = { .fph_ops = &fph_ops, .extra_hdrs = hdrs };
+const struct mtd_fph_ops fph_ops = {
+        .fph_user = my_user,
+        .fph_version = my_ver
+};
+const struct mtd_cfg cfg = {
+        .fph_ops = &fph_ops,
+        .extra_hdrs = hdrs,
+        .config_dir = "/home/foo/.config/mtd-cli"
+};
 
 hdrs[0] = "Gov-Test-Scenario: AGENT_NOT_SUBSCRIBED";
 err = mtd_init(flags, &cfg);

man/man3/mtd_init_config.3
@@ -12,8 +12,9 @@ mtd_init_config \- initialise config
 
 .SH DESCRIPTION
 
-This function is used to create an initial \fI~/.config/libmtdac/{prod,test}-api/config.json\fP
-file that contains the users \fIclient_id\fP & \fIclient_secret\fP.
+This function is used to create an initial
+\fI${CONFIG_DIR}/libmtdac/{prod,test}-api/config.json\fP file that contains
+the users \fIclient_id\fP & \fIclient_secret\fP.
 .PP
 This will prompt the user to enter the above tokens after which the config file
 will be created.
@@ -32,7 +33,7 @@ code is returned.
 There was an OS level failure
 
 .SH FILES
-.I ~/.config/libmtdac/{prod,test}-api/config.json
+.I ${CONFIG_DIR}/libmtdac/{prod,test}-api/config.json
 
 .SH SEE ALSO
 

commit e62d630b2ae2c683734f696eb5a696d724814763

mtd: Fix a couple of clang warnings
When compiling with clang it showed the following

  CC    mtd.o
mtd.c:70:30: warning: use of GNU 'missing =' extension in designator [-Wgnu-designator]
        [MTD_ERR_CONFIG_DIR_UNSPEC] {
                                    ^
                                    =
mtd.c:74:31: warning: use of GNU 'missing =' extension in designator [-Wgnu-designator]
        [MTD_ERR_CONFIG_DIR_INVALID] {
                                     ^
                                     =

Fixes: 0860feb ("mtd: Make the config per client")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd.c
@@ -67,11 +67,11 @@ static const struct _mtd_err_map {
 		.estr	= "MTD_ERR_LIB_TOO_OLD",
 		.str	= "Library version too old"
 	},
-	[MTD_ERR_CONFIG_DIR_UNSPEC] {
+	[MTD_ERR_CONFIG_DIR_UNSPEC] = {
 		.estr	= "MTD_ERR_CONFIG_DIR_UNSPEC",
 		.str	= "Config directory not specified"
 	},
-	[MTD_ERR_CONFIG_DIR_INVALID] {
+	[MTD_ERR_CONFIG_DIR_INVALID] = {
 		.estr	= "MTD_ERR_CONFIG_DIR_INVALID",
 		.str	= "Config directory path is invalid"
 	},