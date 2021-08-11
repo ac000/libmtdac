ac000/libmtdac

commit f3bb7cc91ca4f8088aab0488e2d913560340ac3a

Split out the version from API_VER in mtd-*.c
Put the actual version number of the API into it's own #define and add
that into API_VER.

Just makes it a little easier to see at a glance what the actual version
of the API is.

Closes: https://github.com/ac000/libmtdac/issues/22
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd-il.c
@@ -11,7 +11,8 @@
 #include "mtd-il.h"		/* for default (public) visibility */
 #include "endpoints.h"
 
-#define API_VER			"Accept: application/vnd.hmrc.1.0+json"
+#define VERSION		"1.0"
+#define API_VER		"Accept: application/vnd.hmrc." VERSIOn "+json"
 
 /*
  * [PUT ]

src/mtd-bd.c
@@ -11,7 +11,8 @@
 #include "mtd-bd.h"		/* for default (public) visibility */
 #include "endpoints.h"
 
-#define API_VER			"Accept: application/vnd.hmrc.1.0+json"
+#define VERSION		"1.0"
+#define API_VER		"Accept: application/vnd.hmrc." VERSION "+json"
 
 /*
  * [GET ]

src/mtd-biss.c
@@ -11,7 +11,8 @@
 #include "mtd-biss.h"		/* for default (public) visibility */
 #include "endpoints.h"
 
-#define API_VER			"Accept: application/vnd.hmrc.1.0+json"
+#define VERSION		"1.0"
+#define API_VER		"Accept: application/vnd.hmrc." VERSION "+json"
 
 /*
  * [GET ]

src/mtd-bsas.c
@@ -11,7 +11,8 @@
 #include "mtd-bsas.h"		/* for default (public) visibility */
 #include "endpoints.h"
 
-#define API_VER			"Accept: application/vnd.hmrc.2.0+json"
+#define VERSION		"2.0"
+#define API_VER		"Accept: application/vnd.hmrc." VERSION "+json"
 
 /*
  * [POST]

src/mtd-ic.c
@@ -11,7 +11,8 @@
 #include "mtd-ic.h"		/* for default (public) visibility */
 #include "endpoints.h"
 
-#define API_VER			"Accept: application/vnd.hmrc.2.0+json"
+#define VERSION		"2.0"
+#define API_VER		"Accept: application/vnd.hmrc." VERSION "+json"
 
 /*
  * [POST]

src/mtd-id.c
@@ -11,7 +11,8 @@
 #include "mtd-id.h"		/* for default (public) visibility */
 #include "endpoints.h"
 
-#define API_VER			"Accept: application/vnd.hmrc.1.0+json"
+#define VERSION		"1.0"
+#define API_VER		"Accept: application/vnd.hmrc." VERSION "+json"
 
 /*
  * [DELETE]

src/mtd-ob.c
@@ -11,7 +11,8 @@
 #include "mtd-ob.h"		/* for default (public) visibility */
 #include "endpoints.h"
 
-#define API_VER			"Accept: application/vnd.hmrc.1.0+json"
+#define VERSION		"1.0"
+#define API_VER		"Accept: application/vnd.hmrc." VERSION "+json"
 
 /*
  * [GET ]

src/mtd-sa.c
@@ -11,7 +11,8 @@
 #include "mtd-sa.h"		/* for default (public) visibility */
 #include "endpoints.h"
 
-#define API_VER			"Accept: application/vnd.hmrc.2.0+json"
+#define VERSION		"2.0"
+#define API_VER		"Accept: application/vnd.hmrc." VERSION "+json"
 
 /*
  * [POST]

commit 4a1ac889d1410ca0e276d59b749c48979592aef2

mtd-il: Fix the build
The previous commit f3bb7cc ("Split out the version from API_VER in
mtd-*.c") inadvertently broke the build due to a typo.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/mtd-il.c
@@ -12,7 +12,7 @@
 #include "endpoints.h"
 
 #define VERSION		"1.0"
-#define API_VER		"Accept: application/vnd.hmrc." VERSIOn "+json"
+#define API_VER		"Accept: application/vnd.hmrc." VERSION "+json"
 
 /*
  * [PUT ]