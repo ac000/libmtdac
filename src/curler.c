ac000/libmtdac

commit 56aad55eec7f0500a23105a0ef36cf4f9054a4e8

Flow some extra response headers from HMRC through
When an API is in the process of being deprecated, HMRC will flow the
following response headers through

Deprecation	The deprecation date/time for this endpoint.

Sunset		(optional) The earliest date/time this endpoint will
		become unavailable after deprecation.

Link		Documentation URL for the relevant API.

These will be shown in the main response JSON as either the value as
returned by HMRC or 'null' if they are not set.

Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

src/curler.c
@@ -155,19 +155,21 @@ static size_t header_cb(char *buffer, size_t size, size_t nitems,
 			void *userdata)
 {
 	struct curl_ctx *ctx = userdata;
-	bool found_hdr = true;
 	char *hdr_val;
 	char **ptr;
 	size_t ret = nitems * size;
 
 	if (strcasestr(buffer, "Location:"))
 		ptr = &ctx->accepted_location;
-	else if (strcasestr(buffer, "X-Correlationid"))
+	else if (strcasestr(buffer, "X-Correlationid:"))
 		ptr = &ctx->x_corr_id;
+	else if (strcasestr(buffer, "Deprecation:"))
+		ptr = &ctx->deprecation_hdr;
+	else if (strcasestr(buffer, "Sunset:"))
+		ptr = &ctx->sunset_hdr;
+	else if (strcasestr(buffer, "Link:"))
+		ptr = &ctx->link_hdr;
 	else
-		found_hdr = false;
-
-	if (!found_hdr)
 		return ret;
 
 	hdr_val = strchr(buffer, ' ');
@@ -261,13 +263,16 @@ static void set_response(struct curl_ctx *ctx)
 		json_decref(loc);
 	}
 
-	new = json_pack("{s:i, s:s, s:s, s:s, s:s, s:s?, s:s, s:o?}",
+	new = json_pack("{s:i, s:s, s:s, s:s, s:s, s:s?, s:s?, s:s?, s:s?, s:s, s:o?}",
 			"status_code", ctx->status_code,
 			"status_str", http_status_code2str(ctx->status_code),
 			"libmtdac_endpoint", ctx->epstr,
 			"url", ctx->url,
 			"method", methods_str[ctx->http_method].str,
 			"xid", ctx->x_corr_id,
+			"deprecation", ctx->deprecation_hdr,
+			"sunset", ctx->sunset_hdr,
+			"link", ctx->link_hdr,
 			"date", gen_datestamp(date, sizeof(date)),
 			"result", rootbuf);
 

src/curler.h
@@ -45,6 +45,9 @@ struct curl_ctx {
 	const char *url;
 	char *location;
 	char *x_corr_id;
+	char *deprecation_hdr;
+	char *sunset_hdr;
+	char *link_hdr;
 
 	const char *mtd_api_ver;
 

commit a7501fdcc1c8b4d0f6c3b3b01a3061ed44a19863

Avoid some potential memory leaks
free(3) the deprecation, sunset and link header values we get back from
curl after inserting them into the result JSON.

Handle them like we do for x_corr_id.

Fixes: 56aad55 ("Flow some extra response headers from HMRC through")
Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

src/curler.c
@@ -286,8 +286,16 @@ static void set_response(struct curl_ctx *ctx)
 	json_array_append_new(resbuf, new);
 
 	free(ctx->res_buf);
+
 	free(ctx->x_corr_id);
+	free(ctx->deprecation_hdr);
+	free(ctx->sunset_hdr);
+	free(ctx->link_hdr);
 	ctx->x_corr_id = NULL;
+	ctx->deprecation_hdr = NULL;
+	ctx->sunset_hdr = NULL;
+	ctx->link_hdr = NULL;
+
 	ctx->res_buf = json_dumps(resbuf, JSON_INDENT(4));
 
 	logger(MTD_LOG_DEBUG_DATA, "[%s]\n", ctx->res_buf);