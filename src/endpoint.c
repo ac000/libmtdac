ac000/libmtdac

commit ab39737cff43321bb9dde69230edef665e7027ee

This is basically a new version
The main change is that instead of all the individual API endpoint
functions that were just a straight wrapper around do_ep(), you just
call do_ep() directly now.

Hopefully this will be easier to maintain and keep updated going
forward.

As such, do_ep() has been renamed to mtd_ep(), with the following
prototype

  int mtd_ep(enum mtd_api_endpoint ep, const struct mtd_dsrc_ctx *dsctx,
             char **buf, const char * const params[]);

(it has changed a bit from the old do_ep())

ep is one of the values from enum mtd_api_endpoint (in libmtdac/mtd.h)
This is what says what endpoint to call.

dsctx is either a pointer to a struct mtd_dsrc_ctx which describes
where the POST/PUT data is to be found or NULL for GET/DELETE endpoints.

That is the same from the previous API.

buf is the output buffer for the response and is the same as in the old
API. Dynamically allocated by libmtdac free(3)'s by you.

params is an array of char pointers containing the various arguments
required by the endpoint including any query string.

This array does not need to be NULL terminated as the number of params
is determined by the endpoint in question. But you do need to account
for any optional or not query string. Passing an array with more entries
than required (say NULLs) is fine as any extra entries are ignored.

Any query string should be the last parameter in the array.

If you are accessing an endpoint that takes an optional query string but
don't provide one you must pass a NULL pointer instead.

İ.e. If an endpoint takes two required parameters (that become part of
the URL) and an optional query string, then your params array should
have three entries, with the third entry either pointing to a query
string or NULL.

With this change most of the docs have been removed. New ones will be
added in time.

All the other API remains the same.

Also, as before, you don't pass in {nino} to any endpoints as that's
read from the nino.json when required.

src/endpoint.c
@@ -1,0 +1,142 @@
+/* SPDX-License-Identifier: LGPL-2.1 */
+
+/*
+ * endpoint.c - Make Tax Digital - Self Assessment API
+ *
+ * Copyright (C) 2020 - 2025	Andrew Clayton <ac@sigsegv.uk>
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "mtd.h"
+#include "endpoint.h"
+#include "api_endpoints.h"
+#include "curler.h"
+#include "auth.h"
+#include "logger.h"
+#include "mtd-priv.h"
+
+#define API_VER_FMT         "Accept: application/vnd.hmrc.%s+json"
+
+static const char * const api_version_map[] = {
+	[EP_API_BD]		= "1.0",
+	[EP_API_BSAS]		= "6.0",
+	[EP_API_ICAL]		= "7.0",
+	[EP_API_ILOS]		= "5.0",
+	[EP_API_ISI]		= "1.0",
+	[EP_API_OB]		= "3.0",
+	[EP_API_PB]		= "5.0",
+	[EP_API_SEB]		= "4.0",
+
+	[EP_API_TEST_CU]	= "1.0",
+	[EP_API_TEST_FPH]	= "1.0",
+};
+
+int mtd_ep(enum mtd_api_endpoint ep, const struct mtd_dsrc_ctx *dsctx,
+	   char **buf, const char * const params[])
+{
+	size_t len;
+	struct curl_ctx ctx = {};
+	char api_ver[128];
+
+	*buf = NULL;
+
+	len = snprintf(api_ver, sizeof(api_ver), API_VER_FMT,
+		       api_version_map[endpoints[ep].api]);
+	if (len >= sizeof(api_ver))
+	    return -MTD_ERR_REQUEST;
+
+	ctx.mtd_api_ver = api_ver;
+	ctx.endpoint = ep;
+	ctx.scope = endpoints[ep].scope;
+	ctx.params = params;
+	ctx.content_type = endpoints[ep].ctype;
+	ctx.http_method = endpoints[ep].method;
+	ctx.dsctx = dsctx;
+
+	switch (ctx.http_method) {
+	case M_GET:
+		return do_get(&ctx, buf);
+	case M_POST:
+		return do_post(&ctx, buf);
+	case M_PUT:
+		return do_put(&ctx, buf);
+	case M_DELETE:
+		return do_delete(&ctx, buf);
+	default:
+		return -MTD_ERR_REQUEST;
+	}
+}
+
+int (*ep_set_oauther(enum mtd_api_endpoint ep))(enum mtd_api_scope)
+{
+	switch (endpoints[ep].authz) {
+	case AUTHZ_USER:
+		return oauther_refresh_access_token;
+	case AUTHZ_APPLICATION:
+		return oauther_get_application_token;
+	default:
+		return oauther_dummy;
+	}
+}
+
+char *ep_get_token(enum mtd_api_endpoint ep)
+{
+	switch(endpoints[ep].authz) {
+	case AUTHZ_USER:
+		return load_token("access_token", FT_AUTH, endpoints[ep].scope);
+	case AUTHZ_APPLICATION:
+		return load_token("access_token", FT_AUTH_APPLICATION,
+				  MTD_API_SCOPE_NULL);
+	default:
+		return NULL;
+	}
+}
+
+extern __thread struct mtd_ctx mtd_ctx;
+char *ep_make_url(enum mtd_api_endpoint ep, const char * const params[],
+		  char *url)
+{
+	char *nino = NULL;
+	char *string;
+	char *ptr;
+	int len;
+
+	string = strdup(endpoints[ep].tmpl + 1); /* skip past first '/' */
+	ptr = string;
+
+	len = snprintf(url, URL_LEN + 1, "%s", mtd_ctx.api_url);
+
+	if (strstr(string, "{nino}"))
+		nino = load_token("nino", FT_NINO, MTD_API_SCOPE_NULL);
+
+	for (int p = 0; ;) {
+		char *token;
+
+		token = strsep(&ptr, "/");
+		if (!token)
+			break;
+
+		if (*token != '{')
+			snprintf(url + len, URL_LEN+1 - len, "/%s", token);
+		else if (strcmp(token, "{nino}") == 0)
+			snprintf(url + len, URL_LEN+1 - len, "/%s", nino);
+		else if (strstr(token, "query_params"))
+			snprintf(url + len, URL_LEN+1 - len, "%s",
+				 params[p] ? params[p++] : "");
+		else
+			snprintf(url + len, URL_LEN+1 - len, "/%s",
+				 params[p++]);
+
+		len = strlen(url);
+	}
+
+	free(string);
+	free(nino);
+
+	return url;
+}

commit 32df5a9128697386130bab020d44371af5df99c2

endpoint: Don't set api_ver_hdr for MTD_API_SCOPE_NULL scope
When the API scope is MTD_API_SCOPE_NULL
api_default_values[]->api_version (and ->api_name) will be NULL.

Passing NULL to snprintf(3) et al is undefined behaviour, we were lucky
in that we were getting "(null)" rather than a segfault.

In these cases we don't need to set api_ver_hdr as we don't set the
"Accept" header for "/oauth/token" urls.

Fixes: ab39737 ("This is basically a new version")
Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

src/endpoint.c
@@ -62,17 +62,20 @@ int mtd_ep(enum mtd_api_endpoint ep, const struct mtd_dsrc_ctx *dsctx,
 
 	*buf = NULL;
 
-	len = snprintf(api_ver_hdr, sizeof(api_ver_hdr), API_VER_FMT,
-		       api_default_values[api].api_version);
-	if (len >= sizeof(api_ver_hdr))
-	    return MTD_ERR_REQUEST;
+	ctx.scope = get_scope(ep);
+
+	if (ctx.scope != MTD_API_SCOPE_NULL) {
+		len = snprintf(api_ver_hdr, sizeof(api_ver_hdr), API_VER_FMT,
+			       api_default_values[api].api_version);
+		if (len >= sizeof(api_ver_hdr))
+			return MTD_ERR_REQUEST;
+	}
 
 	ctx.mtd_api_ver_hdr = api_ver_hdr;
 	ctx.mtd_api_name = api_default_values[api].name;
 	ctx.mtd_api_ver = api_default_values[api].api_version;
 	ctx.endpoint = ep;
 	ctx.epstr = endpoints[ep].epstr;
-	ctx.scope = get_scope(ep);
 	ctx.params = params;
 	ctx.content_type = endpoints[ep].ctype;
 	ctx.http_method = endpoints[ep].method;