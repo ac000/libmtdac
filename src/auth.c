ac000/libmtdac

commit ab39737cff43321bb9dde69230edef665e7027ee

This is basically a new version
The main change is that instead of all the individual API endpoint
functions that were just a straight wrapper around do_ep(), you just
call do_ep() directly now.

Hopefully this will be easier to maintain and keep updated going
forward.

As such, do_ep() has been renamed to mtd_ep(), with the following
prototype

  int mtd_ep(enum mtd_api_endpoint ep, const struct mtd_dsrc_ctx *dsctx,
             char **buf, const char * const params[]);

(it has changed a bit from the old do_ep())

ep is one of the values from enum mtd_api_endpoint (in libmtdac/mtd.h)
This is what says what endpoint to call.

dsctx is either a pointer to a struct mtd_dsrc_ctx which describes
where the POST/PUT data is to be found or NULL for GET/DELETE endpoints.

That is the same from the previous API.

buf is the output buffer for the response and is the same as in the old
API. Dynamically allocated by libmtdac free(3)'s by you.

params is an array of char pointers containing the various arguments
required by the endpoint including any query string.

This array does not need to be NULL terminated as the number of params
is determined by the endpoint in question. But you do need to account
for any optional or not query string. Passing an array with more entries
than required (say NULLs) is fine as any extra entries are ignored.

Any query string should be the last parameter in the array.

If you are accessing an endpoint that takes an optional query string but
don't provide one you must pass a NULL pointer instead.

İ.e. If an endpoint takes two required parameters (that become part of
the URL) and an optional query string, then your params array should
have three entries, with the third entry either pointing to a query
string or NULL.

With this change most of the docs have been removed. New ones will be
added in time.

All the other API remains the same.

Also, as before, you don't pass in {nino} to any endpoints as that's
read from the nino.json when required.

src/auth.c
@@ -17,14 +17,14 @@
 
 #include "mtd.h"
 #include "auth.h"
-#include "endpoints.h"
+#include "endpoint.h"
 #include "curler.h"
 #include "logger.h"
 #include "mtd-priv.h"
 
 extern __thread struct mtd_ctx mtd_ctx;
 
-char *load_token(const char *which, enum file_type type, enum mtd_ep_api api)
+char *load_token(const char *which, enum file_type ft, enum mtd_api_scope scope)
 {
 	char path[PATH_MAX];
 	char *token;
@@ -33,7 +33,7 @@ char *load_token(const char *which, enum file_type type, enum mtd_ep_api api)
 	json_t *tok_obj;
 	json_error_t error;
 
-	switch (type) {
+	switch (ft) {
 	case FT_AUTH:
 		file = "oauth.json";
 		break;
@@ -51,7 +51,7 @@ char *load_token(const char *which, enum file_type type, enum mtd_ep_api api)
 	snprintf(path, sizeof(path), "%s/%s", mtd_ctx.config_dir, file);
 
 	root = json_load_file(path, 0, &error);
-	if (!root && type != FT_AUTH_APPLICATION) {
+	if (!root && ft != FT_AUTH_APPLICATION) {
 		/*
 		 * Don't bother displaying this error message for the
 		 * auth-application.json file as this can just be
@@ -64,7 +64,7 @@ char *load_token(const char *which, enum file_type type, enum mtd_ep_api api)
 		return NULL;
 	}
 	tok_obj = root;
-	if (type == FT_CREDS && api == MTD_EP_API_NULL) {
+	if (ft == FT_CREDS && scope == MTD_API_SCOPE_NULL) {
 		/*
 		 * Just take whichever is available.
 		 *
@@ -79,12 +79,12 @@ char *load_token(const char *which, enum file_type type, enum mtd_ep_api api)
 		 * APIs under both sets of credentials.
 		 */
 		tok_obj = json_object_get(root,
-					  ep_api_map[MTD_EP_API_ITSA].name);
+					  api_scope_map[MTD_API_SCOPE_ITSA].name);
 		if (!tok_obj)
 			tok_obj = json_object_get(root,
-						  ep_api_map[MTD_EP_API_VAT].name);
-	} else if (api != MTD_EP_API_NULL) {
-		tok_obj = json_object_get(root, ep_api_map[api].name);
+						  api_scope_map[MTD_API_SCOPE_VAT].name);
+	} else if (scope != MTD_API_SCOPE_NULL) {
+		tok_obj = json_object_get(root, api_scope_map[scope].name);
 	}
 	tok_obj = json_object_get(tok_obj, which);
 	if (!tok_obj)
@@ -95,15 +95,15 @@ char *load_token(const char *which, enum file_type type, enum mtd_ep_api api)
 	return token;
 }
 
-int oauther_refresh_access_token(enum mtd_ep_api api)
+int oauther_refresh_access_token(enum mtd_api_scope scope)
 {
 	struct mtd_dsrc_ctx dsctx;
 	char data[4096];
 	char path[PATH_MAX];
 	char *buf;
-	char *refresh_token = load_token("refresh_token", FT_AUTH, api);
-	char *client_id = load_token("client_id", FT_CREDS, api);
-	char *client_secret = load_token("client_secret", FT_CREDS, api);
+	char *refresh_token = load_token("refresh_token", FT_AUTH, scope);
+	char *client_id = load_token("client_id", FT_CREDS, scope);
+	char *client_secret = load_token("client_secret", FT_CREDS, scope);
 	json_t *array;
 	json_t *froot;
 	json_t *result;
@@ -116,7 +116,7 @@ int oauther_refresh_access_token(enum mtd_ep_api api)
 	dsctx.data_src.buf = data;
 	dsctx.data_len = strlen(data);
 	dsctx.src_type = MTD_DATA_SRC_BUF;
-	err = do_ep(OA_REFRESH_TOKEN, NULL, &dsctx, &buf, (char *)NULL);
+	err = mtd_ep(MTD_API_EP_OA_REFRESH_TOKEN, &dsctx, &buf, NULL);
 	if (err) {
 		logger(MTD_LOG_ERR, "%s\n", buf);
 		goto out_free;
@@ -130,9 +130,9 @@ int oauther_refresh_access_token(enum mtd_ep_api api)
 
 	froot = json_load_file(path, 0, NULL);
 	if (froot)
-		json_object_set(froot, ep_api_map[api].name, result);
+		json_object_set(froot, api_scope_map[scope].name, result);
 	else
-		froot = json_pack("{s:o}", ep_api_map[api].name, result);
+		froot = json_pack("{s:o}", api_scope_map[scope].name, result);
 
 	write_config(mtd_ctx.config_dir, "oauth.json", froot);
 	json_decref(array);
@@ -147,14 +147,14 @@ out_free:
 	return err;
 }
 
-int oauther_get_application_token(enum mtd_ep_api api __unused)
+int oauther_get_application_token(enum mtd_api_scope scope __unused)
 {
 	struct mtd_dsrc_ctx dsctx;
 	char data[4096];
 	char *buf;
-	char *client_id = load_token("client_id", FT_CREDS, MTD_EP_API_NULL);
+	char *client_id = load_token("client_id", FT_CREDS, MTD_API_SCOPE_NULL);
 	char *client_secret = load_token("client_secret", FT_CREDS,
-					 MTD_EP_API_NULL);
+					 MTD_API_SCOPE_NULL);
 	json_t *array;
 	json_t *root;
 	json_t *result;
@@ -167,7 +167,7 @@ int oauther_get_application_token(enum mtd_ep_api api __unused)
 	dsctx.data_src.buf = data;
 	dsctx.data_len = strlen(data);
 	dsctx.src_type = MTD_DATA_SRC_BUF;
-	err = do_ep(OA_APPLICATION_TOKEN, NULL, &dsctx, &buf, (char *)NULL);
+	err = mtd_ep(MTD_API_EP_OA_APPLICATION_TOKEN, &dsctx, &buf, NULL);
 	if (err) {
 		logger(MTD_LOG_ERR, "%s\n", buf);
 		goto out_free;
@@ -187,7 +187,7 @@ out_free:
 	return err;
 }
 
-int oauther_dummy(enum mtd_ep_api api __unused)
+int oauther_dummy(enum mtd_api_scope scope __unused)
 {
 	return 0;
 }

commit e312635b6bf727e0676aa355f70cf1f2cdf521b4

auth: Properly error out when failing to load JSON in load_token()
Fixes: ab39737 ("This is basically a new version")
Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

src/auth.c
@@ -57,16 +57,19 @@ char *load_token(const char *which, enum file_type ft, enum mtd_api_scope scope)
 	snprintf(path, sizeof(path), "%s/%s", mtd_ctx.config_dir, file);
 
 	root = json_load_file(path, 0, &error);
-	if (!root && ft != FT_AUTH_APPLICATION) {
-		/*
-		 * Don't bother displaying this error message for the
-		 * auth-application.json file as this can just be
-		 * re-created.
-		 *
-		 * This will happen for instance on a fresh setup with no
-		 * config files and creating a test user.
-		 */
-		logger(MTD_LOG_ERR, "json_load_file: %s\n", error.text);
+	if (!root) {
+		if (ft != FT_AUTH_APPLICATION) {
+			/*
+			 * Don't bother displaying this error message for the
+			 * auth-application.json file as this can just be
+			 * re-created.
+			 *
+			 * This will happen for instance on a fresh setup with
+			 * no config files and creating a test user.
+			 */
+			logger(MTD_LOG_ERR, "json_load_file: %s\n", error.text);
+		}
+
 		return NULL;
 	}
 	tok_obj = root;